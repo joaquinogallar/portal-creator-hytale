d650e29080ae0318327e86fd69c906d8d371587c
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-01-25 16:24:56 -0300

    feat: main plugin file where components, systems, and commands are registered

---
+++ src/main/java/com/joaquinogallar/portalcreator/PortalCreatorPlugin.java
@@ -0,0 +1,15 @@
+package com.joaquinogallar.portalcreator;
+
+import com.hypixel.hytale.server.core.plugin.JavaPlugin;
+import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
+
+import javax.annotation.Nonnull;
+
+public class PortalCreatorPlugin extends JavaPlugin {
+    public PortalCreatorPlugin(@Nonnull JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {}
+}
