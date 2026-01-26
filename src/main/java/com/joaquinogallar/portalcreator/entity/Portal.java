9a855dd6b4be024962cc3812e57dae76727237f9
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-01-26 08:41:50 -0300

    feat: implemented getQuery System method and created addPortal method

--- src/main/java/com/joaquinogallar/portalcreator/entity/Portal.java
+++ src/main/java/com/joaquinogallar/portalcreator/entity/Portal.java
@@ -3,7 +3,7 @@ package com.joaquinogallar.portalcreator.entity;
 import com.hypixel.hytale.math.vector.Vector3d;
 
 public record Portal(
-        String portalName,
+        String name,
         Vector3d p1,
         Vector3d p2,
         Vector3d destination
--- src/main/java/com/joaquinogallar/portalcreator/system/PortalCreatorSystem.java
+++ src/main/java/com/joaquinogallar/portalcreator/system/PortalCreatorSystem.java
@@ -5,6 +5,9 @@ import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.tick.EntityTickingSystem;
+import com.hypixel.hytale.math.vector.Vector3d;
+import com.hypixel.hytale.server.core.modules.entity.component.TransformComponent;
+import com.hypixel.hytale.server.core.universe.PlayerRef;
 import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 import com.joaquinogallar.portalcreator.entity.Portal;
 
@@ -17,6 +20,16 @@ public class PortalCreatorSystem extends EntityTickingSystem<EntityStore> {
 
     private static final List<Portal> portals = new ArrayList<>();
 
+    public static boolean addPortal(String name, Vector3d p1, Vector3d p2, Vector3d destination) {
+        for (Portal p : portals) {
+            if(p.name().equals(name)) {
+                return false;
+            }
+        }
+        portals.add(new Portal(name, p1, p2, destination));
+        return true;
+    }
+
     @Override
     public void tick(float v, int i, @Nonnull ArchetypeChunk<EntityStore> archetypeChunk, @Nonnull Store<EntityStore> store, @Nonnull CommandBuffer<EntityStore> commandBuffer) {
 
@@ -25,6 +38,9 @@ public class PortalCreatorSystem extends EntityTickingSystem<EntityStore> {
     @Nullable
     @Override
     public Query<EntityStore> getQuery() {
-        return null;
+        return Query.and(
+                PlayerRef.getComponentType(),
+                TransformComponent.getComponentType()
+        );
     }
 }
