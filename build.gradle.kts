474be7e7cb4b9e7e3d3fffe0c68ba778f8cb995e
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-04-23 23:28:00 -0300

    feat: change local dependency to global

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,11 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://maven.hytale.com/release")
 }
 
 dependencies {
-    compileOnly(files("libs/HytaleServer.jar"))
+    compileOnly("com.hypixel.hytale:Server:2026.03.26-89796e57b")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
